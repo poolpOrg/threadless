b6990b53719a72ec2e5a856361c3f16eb6692dcf
gilles <gilles@poolp.org>
2016-06-14 08:25:44 +0200

    - add .gitignore - add simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='threadless',
+    version='0.0.1',
+    description='A framework to ease writing asynchronous agents',
+    url='http://github.com/poolpOrg/threadless.git',
+    author='Gilles Chehade',
+    author_email='gilles@poolp.org',
+    license='ISC',
+    packages=['threadless'],
+    zip_safe=False)
